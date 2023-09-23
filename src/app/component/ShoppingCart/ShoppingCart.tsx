63b151e675bbfba460b815f122b34a348f7ec9b9
rishiTamoghna17 <tamoghna171099@gmail.com>
2023-09-23 22:59:53 +0530

    cart page

--- src/app/component/ShoppingCart/ShoppingCart.tsx
+++ src/app/component/ShoppingCart/ShoppingCart.tsx
@@ -39,6 +39,24 @@ const dummyCartData: CartItem[] = [
     discount: 60,
     quantity: 1,
   },
+  {
+    id: 4,
+    name: "Product 3",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "/DSC_3003.JPG",
+    price: "24.99",
+    discount: 60,
+    quantity: 1,
+  },
+  {
+    id: 5,
+    name: "Product 3",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "/DSC_3003.JPG",
+    price: "24.99",
+    discount: 60,
+    quantity: 1,
+  },
   // Add more cart items here
 ];
 
@@ -74,65 +92,122 @@ const ShoppingCart: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto mt-8">
-      <div className="flex flex-row items-center w-full justify-between ">
-        <div className="flex items-center flex-col w-full">
-          <div className="flex items-center justify-center w-full">
+    <div className="container mx-auto my-8">
+      <div className="flex items-center px-5">
             <h2 className="text-3xl font-extrabold text-gray-800 mb-6">
-              Shopping Cart
+              My Bag
+              <span className="text-2xl font-bold">
+                {" "}
+                {cartItems.length} items
+              </span>
             </h2>
           </div>
-          <div className="flex flex-col ">
-            <div className="flex flex-col gap-6">
+      <div className="flex flex-row  w-full justify-between ">
+        <div className="flex items-center flex-col w-[60%] ml-5">
+          
+          <div className="flex flex-col w-full">
+            <div className="flex flex-col gap-6 w-full">
               {cartItems.map((item) => (
                 <div
                   key={item.id}
-                  className="bg-white flex flex-col items-center rounded-lg shadow-md p-4"
+                  className="bg-white flex flex-row  justify-between rounded-lg shadow-md p-4"
                 >
-                  <Image
-                    src={item.image}
-                    alt={item.name}
-                    width={320}
-                    height={320}
-                    className=" object-cover mx-auto rounded-t-lg mb-4"
-                  />
-                  <h3 className="text-lg font-semibold text-gray-800">
-                    {item.name}
-                  </h3>
-                  <p className="text-gray-600">Price: ${item.price}</p>
-                  <div className="flex items-center mt-2">
-                    <button
-                      className="text-blue-500 hover:text-blue-600 focus:outline-none"
-                      onClick={() => decreaseQuantity(item.id)}
-                    >
-                      -
-                    </button>
-                    <span className="mx-2">{item.quantity}</span>
-                    <button
-                      className="text-blue-500 hover:text-blue-600 focus:outline-none"
-                      onClick={() => increaseQuantity(item.id)}
-                    >
-                      +
-                    </button>
-                    <button
-                      className="text-red-500 hover:text-red-600 ml-4 focus:outline-none"
-                      onClick={() => removeItem(item.id)}
-                    >
-                      Remove
-                    </button>
+                  <div className="flex flex-col items-center">
+                    <Image
+                      src={item.image}
+                      alt={item.name}
+                      width={320}
+                      height={320}
+                      className=" object-cover mx-auto rounded-t-lg mb-4"
+                    />
+                    <h3 className="text-lg font-semibold text-gray-800">
+                      {item.name}
+                    </h3>
+                  </div>
+                  <div className="flex flex-col items-end gap-5">
+                    <p>{item.description}</p>
+                    <div className="flex items-center ml-1">
+                    <span className="text-xl font-bold ">
+                      $
+                      {(
+                        parseFloat(item.price) *
+                        (1 - item.discount / 100)
+                      ).toFixed(2)}
+                    </span>
+                    <span className="text-sm font-bold text-slate-500 line-through m-2">
+                      ${Number(item.price).toFixed(2)}
+                    </span>
+                    <span className="text-lg font-semibold text-red-500 m-2">
+                      {(item.discount).toFixed(2)}% OFF
+                    </span>
+                  </div>
+                    <div className="flex items-center mt-2 p-1 justify-center  bg-gray-100 rounded">
+                      <span className="mx-5 text-sm font-bold capitalize tracking-widest">qty :</span>
+                      <button
+                        className="text-blue-500 hover:text-blue-600 focus:outline-none"
+                        onClick={() => decreaseQuantity(item.id)}
+                      >
+                        -
+                      </button>
+                      <span className="mx-2">{item.quantity}</span>
+                      <button
+                        className="text-blue-500 hover:text-blue-600 focus:outline-none mr-2"
+                        onClick={() => increaseQuantity(item.id)}
+                      >
+                        +
+                      </button>
+                      </div>
+                      <button
+                        className="text-red-500 hover:text-red-600 ml-4 text-sm hover:bg-gray-100 font-semibold focus:outline-none p-2 border rounded "
+                        onClick={() => removeItem(item.id)}
+                      >
+                        Remove
+                      </button>
+                    
                   </div>
                 </div>
               ))}
             </div>
           </div>
         </div>
-        <div className="mt-6">
-          <p className="text-xl font-semibold text-gray-800">
-            Total Price: ${getTotalPrice().toFixed(2)}
+        <div className="border border-gray-300"></div>
+        <div className="mt-6 border-2 mr-10 rounded h-[300px] w-[350px] flex flex-col justify-between shadow-md">
+          <div className="flex items-center justify-center p-2 h-10 bg-gray-300">
+            <span className="mx-5 text-xl font-medium capitalize tracking-widest">
+            price summery
+            </span>
+          </div>
+          <div className="flex items-center justify-between p-2">
+            <p className=" font-semibold text-xl text-gray-500 ">Total MRP :-</p>
+          <p className="text-xl font-semibold text-gray-500 ">
+             ${getTotalPrice().toFixed(2)}
           </p>
-          <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full transition duration-300 transform hover:scale-105 focus:outline-none focus:ring focus:ring-blue-300">
+          </div>
+          <div className="flex items-center justify-between p-2">
+            <p className=" font-semibold text-xl text-gray-500 ">Shipping Charges :-</p>
+          <p className="text-xl font-semibold text-gray-500 ">
+             ${getTotalPrice().toFixed(2)}
+          </p>
+          </div>
+          <div className="flex items-center justify-between p-2">
+            <p className=" font-semibold text-xl text-gray-500 ">Discount :-</p>
+          <p className="text-xl font-semibold text-gray-500 ">
+             ${getTotalPrice().toFixed(2)}
+          </p>
+          </div>
+          <div className="flex items-center justify-between p-2">
+            <p className=" font-semibold text-xl text-gray-800 ">Sub Price :-</p>
+          <p className="text-xl font-semibold text-gray-800 ">
+             ${getTotalPrice().toFixed(2)}
+          </p>
+          </div>
+          <div className="border border-gray-300"></div>
+          <div className="flex flex-row items-center justify-between p-2">
+            <p className="font-semibold text-xl text-gray-500">Total - <span className="font-semibold text-xl text-gray-800">${getTotalPrice().toFixed(2)}</span></p>
+          <button className=" bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-[10px] transition duration-300 transform hover:scale-105 focus:outline-none focus:ring focus:ring-blue-300">
             Checkout
           </button>
+          </div>
         </div>
       </div>
     </div>
